d2389073aa0bc2e84acb0f271dcb7e2eba51bea1
Javier G. Leal <jgleal@gmail.com>
2018-03-14 11:58:08 +0100

    geolink apple

--- js/mapas.js
+++ js/mapas.js
@@ -42,16 +42,16 @@ let lyGPS = new M.layer.GeoJSON({
 			color: function (feature) {
 				let h = hermandades.getByField("etiqueta_gps", feature.getAttribute("name"));
 				if (h != null) {
-				    return h.color;
+					return h.color;
 				} else {
 					return "#000";
 				}
 			},
-			opacity: function (feature){
-				if (feature.getAttribute("order")==0)//última posición
+			opacity: function (feature) {
+				if (feature.getAttribute("order") == 0) //última posición
 					return 0.7;
 				else
-					return 0.4; 
+					return 0.4;
 			}
 		},
 		stroke: {
@@ -59,14 +59,14 @@ let lyGPS = new M.layer.GeoJSON({
 		},
 		label: {
 			text: function (feature) {
-				if (feature.getAttribute("order")==0) //solo última posición
-				return feature.getAttribute("name") + "\n\r" +
-					formatDate(new Date(feature.getAttribute("ts")), "gps");
-				else 
-				return formatDate(new Date(feature.getAttribute("ts")), "gps");
+				if (feature.getAttribute("order") == 0) //solo última posición
+					return feature.getAttribute("name") + "\n\r" +
+						formatDate(new Date(feature.getAttribute("ts")), "gps");
+				else
+					return formatDate(new Date(feature.getAttribute("ts")), "gps");
 			},
 			font: "bold 9px arial",
-			offset: [0,-25],
+			offset: [0, -25],
 			fill: {
 				color: "#000"
 			},
@@ -77,7 +77,7 @@ let lyGPS = new M.layer.GeoJSON({
 		}
 	})
 });
-let filtroGPS = M.filter.EQUAL("order",0);
+let filtroGPS = M.filter.EQUAL("order", 0);
 let lyRuta = new M.layer.GeoJSON({
 	name: "Rutas",
 	extract: false
@@ -105,22 +105,22 @@ let lyRutaDiario = new M.layer.GeoJSON({
 
 function createMaps() {
 	mapajsRuta = M.map({
-		controls: ["location","scale","layerswitcher"],
+		controls: ["location", "scale", "layerswitcher"],
 		container: "mapRuta",
 		wmcfiles: ["romero_mapa", "romero_satelite"],
 		layers: [lyRuta]
 	});
 
 	mapajsDiario = M.map({
-		controls: ["location","scale","layerswitcher"],
+		controls: ["location", "scale", "layerswitcher"],
 		container: "mapDiario",
 		wmcfiles: ["romero_mapa", "romero_satelite"],
 		layers: [lyRutaDiario, lyPois]
 	});
 
-	
+
 	mapajsTopo = M.map({
-		controls: ["location","scale","layerswitcher"],
+		controls: ["location", "scale", "layerswitcher"],
 		container: "mapToponimo",
 		wmcfiles: ["romero_mapa", "romero_satelite"]
 	});
@@ -128,15 +128,15 @@ function createMaps() {
 	mapajsTopo.getLayers({
 		name: "__draw__"
 	})[0].setStyle(poiStyle);
-	
+
 	mapajsOcupados = M.map({
-		controls: ["location","scale","layerswitcher"],
+		controls: ["location", "scale", "layerswitcher"],
 		container: "mapOcupados",
 		wmcfiles: ["romero_mapa", "romero_satelite"]
 	});
 
 	mapajsGPS = M.map({
-		controls: ["location","scale","layerswitcher"],
+		controls: ["location", "scale", "layerswitcher"],
 		container: "mapGPS",
 		wmcfiles: ["romero_mapa", "romero_satelite"],
 		layers: [lyGPS]
@@ -145,9 +145,9 @@ function createMaps() {
 
 function addCaminosOcupados(mapa) {
 	let dateHoy = new Date();
-	let codJornada = dateHoy.getHours() > horaCambioJornada? 2 : 1;
+	let codJornada = dateHoy.getHours() > horaCambioJornada ? 2 : 1;
 	let hoy = dias.find(d => d.fecha == formatDate(dateHoy));
-	let codFecha = (hoy != undefined)? hoy.codigo_fecha : 0;
+	let codFecha = (hoy != undefined) ? hoy.codigo_fecha : 0;
 	let cqlOcupados = `CODIGO_JORNADA='${codJornada}' AND CODIGO_FECHA='${codFecha}'`;
 	let lyCaminosOcupados = new M.layer.WMS({
 		url: urlWMSCaminosOcupados + '?cql_filter=' + encodeURI(cqlOcupados),
@@ -156,39 +156,50 @@ function addCaminosOcupados(mapa) {
 		transparent: true,
 		tiled: false
 	});
-	
+
 	mapa.removeLayers(lyCaminosOcupados).addLayers(lyCaminosOcupados);
 }
 //para controlar que sólo está añadida al mapa actual visualizado
 //ya que mapea no permite una capa en varios mapas
-function establecerMapaGPSlayer(mapa){
+function establecerMapaGPSlayer(mapa) {
 	lyGPS.getImpl().map.removeLayers([lyGPS]);
 	mapa.addLayers(lyGPS);
 
-	if (window.isIOS){
+	if (window.isIOS) {
 		//desactivo de todos los location ya que en iOS parece no poder
 		//compartir la ubicación con múltiples controles. 
-		mapajsOcupados.getControls({name:'location'})[0].deactivate();
-		mapajsRuta.getControls({name:'location'})[0].deactivate();
-		mapajsDiario.getControls({name:'location'})[0].deactivate();
-		mapajsGPS.getControls({name:'location'})[0].deactivate();
-		mapajsTopo.getControls({name:'location'})[0].deactivate();
+		mapajsOcupados.getControls({
+			name: 'location'
+		})[0].deactivate();
+		mapajsRuta.getControls({
+			name: 'location'
+		})[0].deactivate();
+		mapajsDiario.getControls({
+			name: 'location'
+		})[0].deactivate();
+		mapajsGPS.getControls({
+			name: 'location'
+		})[0].deactivate();
+		mapajsTopo.getControls({
+			name: 'location'
+		})[0].deactivate();
 	}
-
-function transformar(arrCoords){
-	var epsg4326 = proj4.defs('EPSG:4326');
-	var epsg25830 = proj4.defs('EPSG:25830');
-	coordTrans = proj4(epsg25830,epsg4326,arrCoords);
-	return coordTrans[1]+","+coordTrans[0];
 }
 
-function getGeoLink(coords,label){
-	if (window.isApp){
+function transformar(arrCoords) {
+	var epsg4326 = proj4.defs('EPSG:4326');
+	var epsg25830 = proj4.defs('EPSG:25830');
+	coordTrans = proj4(epsg25830, epsg4326, arrCoords);
+	return coordTrans[1] + "," + coordTrans[0];
+}
+
+function getGeoLink(coords, label) {
+	if (window.isApp) {
 		if (window.isIOS)
 			return `http://maps.apple.com?ll=${coords}&q=${label}`;
 		else
 			return `geo:${coords}?q=${coords}(${label})`;
-	}else
+	} else
 		return `http://maps.google.com?q=${coords}`;
 
 }
